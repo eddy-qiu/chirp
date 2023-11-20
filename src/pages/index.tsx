07f2e849cf0760818367fb579d0a8fd0efea383f
eddy-qiu <eqiu22@gmail.com>
2023-11-19 23:03:42 -0500

    startup again

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { SignIn, SignInButton } from "@clerk/nextjs";
+import { SignIn, SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
 import Head from "next/head";
 import Link from "next/link";
 import { api } from "~/utils/api";
@@ -6,6 +6,8 @@ import { api } from "~/utils/api";
 export default function Home() {
   const hello = api.example.hello.useQuery({ text: "from tRPC" });
 
+  const user = useUser();
+  
   return (
     <>
       <Head>
@@ -15,7 +17,8 @@ export default function Home() {
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
         <div>
-            <SignInButton />
+            {!user.isSignedIn&&<SignInButton />}
+            {!!user.isSignedIn&&<SignOutButton />}
         </div>
         <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
       </main>
