1e840cb9c7b9ce3ccbc5f27eb25c3bfdd98adfa2
eddy-qiu <eqiu22@gmail.com>
2023-11-19 23:33:05 -0500

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,10 +4,9 @@ import Link from "next/link";
 import { api } from "~/utils/api";
 
 export default function Home() {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
   const user = useUser();
-  
+  const {data} = api.posts.getAll.useQuery();
+
   return (
     <>
       <Head>
@@ -20,7 +19,9 @@ export default function Home() {
             {!user.isSignedIn&&<SignInButton />}
             {!!user.isSignedIn&&<SignOutButton />}
         </div>
-        <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        <div>
+          {data?.map((post) => (<div key={post.id}>{post.content}</div>))}
+        </div>
       </main>
     </>
   );
