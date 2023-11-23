9f4a056e8e8e9982e0c0bbb70dd43008ccb83f11
eddy-qiu <eqiu22@gmail.com>
2023-11-23 00:57:09 -0500

    bugfixes + styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { SignInButton, useUser } from "@clerk/nextjs";
+import { type NextPage } from "next";
 import Head from "next/head";
 
 import { api } from "~/utils/api";
@@ -6,30 +7,33 @@ import type { RouterOutputs } from "~/utils/api";
 
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
+import Image from "next/image";
+import { LoadingPage } from "~/components/loading";
 
 dayjs.extend(relativeTime);
 
-import Image from 'next/image';
-import { LoadingPage, LoadingSpinner } from "~/components/loading";
-
 const CreatePostWizard = () => {
-  
   const { user } = useUser();
+
+  console.log(user);
 
   if (!user) return null;
 
-  return <div className="flex gap-4 w-full">
-    <Image 
-      src= {user.profileImageUrl} 
-      alt="Profile Image" 
-      className="w-14 h-14 rounded-full"
-      width={56}
-      height={56}
-    />
-    <input placeholder="Type some emojis!" className="bg-transparent grow outline-none">
-    </input>
-  </div>
-
+  return (
+    <div className="flex w-full gap-3">
+      <Image
+        src={user.profileImageUrl}
+        alt="Profile image"
+        className="h-14 w-14 rounded-full"
+        width={56}
+        height={56}
+      />
+      <input
+        className="align-bottom grow bg-transparent text-black rounded-full placeholder-slate-500 h-14 bg-slate-200"
+        placeholder="   Type some emojis!"
+      />
+    </div>
+  );
 };
 
 type PostWithUser = RouterOutputs["posts"]["getAll"][number];
@@ -37,47 +41,51 @@ type PostWithUser = RouterOutputs["posts"]["getAll"][number];
 const PostView = (props: PostWithUser) => {
   const { post, author } = props;
   return (
-    <div key={post.id} className="flex flex-row border-b border-white p-4 gap-3">
-      <Image 
-        src={author.profilePicture} 
+    <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
+      <Image
+        src={author.profilePicture}
+        className="h-14 w-14 rounded-full"
         alt={`@${author.username}'s profile picture`}
-        className="w-14 h-14 rounded-full" 
         width={56}
         height={56}
       />
       <div className="flex flex-col">
-        <div className="flex gap-1">
+        <div className="flex gap-1 text-black">
           <span>{`@${author.username} `}</span>
-          <span className="font-thin">{` • ${dayjs(post.createdAt).fromNow()}`}</span>
+          <span className="font-thin">{` · ${dayjs(
+            post.createdAt
+          ).fromNow()}`}</span>
         </div>
         <span>{post.content}</span>
       </div>
     </div>
-  )
-}
+  );
+};
 
 const Feed = () => {
-  const {data, isLoading: postsLoading} = api.posts.getAll.useQuery();
+  const { data, isLoading: postsLoading } = api.posts.getAll.useQuery();
 
-  if (!postsLoading) return <LoadingPage />;
+  if (postsLoading) return <LoadingPage />;
 
   if (!data) return <div>Something went wrong</div>;
 
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col bg-white">
       {[...data, ...data]?.map((fullPost) => (
         <PostView {...fullPost} key={fullPost.post.id} />
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default function Home() {
-  const {user, isLoaded: userLoaded} = useUser();
-  const {data, isLoading: postsLoading} = api.posts.getAll.useQuery();
+const Home: NextPage = () => {
+  const { isLoaded: userLoaded, isSignedIn } = useUser();
 
-  // return empty div if both aren't loaded because user tends to load faster
-  if (!userLoaded && !postsLoading) return <div />;
+  // Start fetching asap
+  api.posts.getAll.useQuery();
+
+  // Return empty div if user isn't loaded
+  if (!userLoaded) return <div />;
 
   return (
     <>
@@ -86,19 +94,22 @@ export default function Home() {
         <meta name="description" content="chirp" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="flex justify-center h-screen">
-        <div className="bg-black border-x border-white w-full md:max-w-2xl">
-          <div className="border-b p-4 border-white">
-              {!user.isSignedIn && (
-                <div className="flex justify-center">
-                  <SignInButton />
-                </div>
-              )}
-              {!!user.isSignedIn && <CreatePostWizard />}
+      <main className="flex h-screen justify-center">
+        <div className="h-full w-full border-x border-slate-400 md:max-w-2xl bg-white">
+          <div className="flex border-b border-slate-400 p-4">
+            {!isSignedIn && (
+              <div className="flex justify-center">
+                <SignInButton />
+              </div>
+            )}
+            {isSignedIn && <CreatePostWizard />}
           </div>
-          
+
+          <Feed />
         </div>
       </main>
     </>
   );
-}
+};
+
+export default Home;
